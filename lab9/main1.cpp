f1b60e8755b48a4da7439177b0162e6527390c4e
Евгений <rabkov_evgeniy@mail.ru>
2021-02-10 01:58:17 +0700

    Finish lab 9

--- lab9/main1.cpp
+++ lab9/main1.cpp
@@ -18,35 +18,51 @@ struct Note {
         }
         cout << endl;
     }
+
+    void SetAll(string firstname, int* arr)
+    {
+        this->firstname = firstname;
+        for (int i = 0; i < 4; i++)
+            this->arr[i] = arr[i];
+    }
 };
 
-struct Node {
-    Node(string firstname, int arr[4])
-    {
-        a.firstname = firstname;
-        for (int i = 0; i < 4; i++) {
-            a.arr[i] = arr[i];
-        }
-        this->next = nullptr;
+bool sr_str(string str1, string str2)
+{
+    for (int i = 0; i < str1.size() && i < str2.size(); i++) {
+        if ((int)str1[i] > (int)str2[i])
+            return 1;
+        if ((int)str1[i] < (int)str2[i])
+            return 0;
+        if ((i + 1) == str1.size())
+            return 1;
     }
+    return 0;
+}
+
+struct Node {
     Note a;
     Node* next;
 };
 
 int main()
 {
-    int n, size = 0;
+    int n, size;
     cin >> n;
-    string firstname;
+    size = n;
+    string input, st;
     int arr[4];
 
     cout << "input firstname and thour bal:" << endl;
-    cin >> firstname;
+    cin >> input;
     for (int i = 0; i < 4; i++) {
         cin >> arr[i];
     }
-    size++;
-    Node* First = new Node(firstname, arr);
+
+    Node* First = new Node;
+    First->next = nullptr;
+    First->a.SetAll(input, arr);
+
     Node* iterator1 = First;
     Node* iterator2 = nullptr;
     Node* min;
@@ -54,31 +70,46 @@ int main()
 
     for (int j = 0; j < n - 1; j++) {
         cout << "input firstname and thour bal:" << endl;
-        cin >> firstname;
+        cin >> input;
         for (int i = 0; i < 4; i++) {
             cin >> arr[i];
         }
-        iterator1->next = new Node(firstname, arr);
+        First = new Node;
+        First->a.SetAll(input, arr);
+        First->next = iterator1;
+        iterator1 = First;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < size; i++) {
+        iterator1->a.show();
         iterator1 = iterator1->next;
-        size++;
     }
 
     iterator1 = First;
+
     for (int i = 0; i < size; i++) {
         min = iterator1;
         iterator2 = iterator1;
         for (int j = i + 1; j < size; j++) {
             iterator2 = iterator2->next;
-            if (min->a.firstname > iterator2->a.firstname)
+            st = min->a.firstname;
+            input = iterator2->a.firstname;
+            if (sr_str(st, input)) {
                 min = iterator2;
+            }
         }
-        temp = iterator1->a;
-        iterator1->a = iterator2->a;
-        iterator2->a = temp;
+
+        temp = iterator1->a;   // swap
+        iterator1->a = min->a; //
+        min->a = temp;         //
+
         iterator1 = iterator1->next;
     }
-
+    cout << endl;
     iterator1 = First;
+
     for (int i = 0; i < size; i++) {
         iterator1->a.show();
         iterator1 = iterator1->next;
